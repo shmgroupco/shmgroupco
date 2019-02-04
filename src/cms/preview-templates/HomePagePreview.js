02881d225970d16949b74ca29bcc8deb3716b7f0
Ahsan Javed <iahsanjaved@yahoo.com>
2019-02-04 16:51:14 +0500

    'update'

--- src/cms/preview-templates/HomePagePreview.js
+++ src/cms/preview-templates/HomePagePreview.js
@@ -1,11 +1,17 @@
 import React from "react";
 import PropTypes from "prop-types";
+import ScriptTag from 'react-script-tag';
 
 import { HomePageTemplate } from "../../pages/index";
 
 const HomePagePreview = ({ entry }) => {
   const home = entry.getIn(["data"]).toJS();
-  return <HomePageTemplate home={home} />;
+  return (
+    <>
+      <ScriptTag isHydrating={true} type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.slim.min.js" />
+      <HomePageTemplate home={home} />
+    </>
+  );
 };
 
 HomePagePreview.propTypes = {
