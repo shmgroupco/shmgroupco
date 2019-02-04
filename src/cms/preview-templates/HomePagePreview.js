403bf7513f711056b4422709799a43cabe3210b9
shmgroupco <46463714+shmgroupco@users.noreply.github.com>
2019-02-05 01:49:42 +0500

    Update HomePagePreview.js

--- src/cms/preview-templates/HomePagePreview.js
+++ src/cms/preview-templates/HomePagePreview.js
@@ -8,7 +8,7 @@ const HomePagePreview = ({ entry }) => {
   const home = entry.getIn(["data"]).toJS();
   return (
     <>
-      <ScriptTag isHydrating={true} type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.slim.min.js" />
+      <ScriptTag isHydrating={false} type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.slim.min.js" />
       <HomePageTemplate home={home} />
     </>
   );
